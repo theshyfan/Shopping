631a863e066db26d7c8e10c30cf52fe8cc95714a
theshyfan <yijie@usc.edu>
2024-03-03 22:21:53 -0700

    add welcome page

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,23 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView,TouchableOpacity } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {Ionicons, Fontisto} from "@expo/vector-icons";
+import styles from "./home.style";
+import { Welcome } from "../components";
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyle}>Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <View style={styles.appBarWrapper}>
+        <View style={styles.appBar}>
+          <Ionicons name='location-outline' size={24} />
 
-export default Home
+          <Text style={styles.location}>Shanghai</Text>
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1
-  },
-  textStyle: {
-    fontFamily: 'bold'
-  }
-})
+          <View style={{  alignItems:"flex-end"}}>
+            <View style={styles.carCount}>
+              <Text style={styles.carNumber}>8</Text>
+            </View>
+            <TouchableOpacity>
+              <Fontisto name="shopping-bag" size={24} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <ScrollView>
+        <Welcome />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
